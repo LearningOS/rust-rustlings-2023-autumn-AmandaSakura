ee6b3b749b1b67f0c7552c0ea22b5a21ea96d024
AmandaSakura <1298018274@qq.com>
2023-10-18 23:48:39 +0800

    update

--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -30,11 +30,13 @@
 // I AM NOT DONE
 
 extern "Rust" {
+    #[no_mangle]
     fn my_demo_function(a: u32) -> u32;
+    #[link_name = "my_demo_function"]
     fn my_demo_function_alias(a: u32) -> u32;
 }
 
-mod Foo {
+mod FOO {
     // No `extern` equals `extern "Rust"`.
     fn my_demo_function(a: u32) -> u32 {
         a
